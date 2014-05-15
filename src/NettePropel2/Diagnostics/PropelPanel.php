745de41e0ed4e973f8a5d18d78e401f1d177d7ec
Livio Ribeiro <livioribeiro@outlook.com>
2014-05-15 16:21:35 -0300

    Compatibility with Nette 2.2

--- src/NettePropel2/Diagnostics/PropelPanel.php
+++ src/NettePropel2/Diagnostics/PropelPanel.php
@@ -2,14 +2,23 @@
 
 namespace NettePropel2\Diagnostics;
 
-use Monolog\Handler\HandlerInterface,
-    Monolog\Handler\AbstractHandler,
-    Nette,
-    Nette\Database\Helpers,
-    Nette\Utils\Strings,
-    Propel\Runtime\Propel;
+if (!interface_exists('Tracy\IBarPanel')) {
+    class_alias('Nette\Diagnostics\IBarPanel', 'Tracy\IBarPanel');
+}
 
-class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel, HandlerInterface {
+use Nette,
+    Nette\Database\Helpers,
+    Nette\Utils\Strings;
+
+use Monolog\Handler\AbstractHandler,
+    Monolog\Handler\HandlerInterface;
+
+use Propel\Runtime\Propel;
+
+use Tracy;
+
+class PropelPanel extends AbstractHandler implements Tracy\IBarPanel, HandlerInterface {
+
     /** @var int */
     public $maxQueries = 100;
 
@@ -32,30 +41,31 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
     public $disabled = FALSE;
 
     public function __construct($name = "default") {
+        parent::__construct();
         $this->name = $name;
     }
 
     public function logQuery($message) {
         if ($this->disabled) {
-			return;
-		}
-        
+            return;
+        }
+
         list($time, $memory, $rows, $query) = explode('|', $message);
-        
+
         $query = trim($query);
-        
+
         if (Strings::startsWith($query, "EXPLAIN")) {
             return;
         }
-        
+
         $this->count++;
-        $time = trim($time, ' SLOW Time: ms');
-        $time = floatval($time);
-        $this->totalTime += $time;
         
+        $time = trim($time, ' SLOW Time: ms');
+        $this->totalTime += floatval($time);
+
         $memory = trim($memory, 'Memory: ');
         $rows = trim($rows, 'Affected Rows: ');
-        
+
         $this->queries[] = [
             'time' => $time,
             'memory' => trim($memory),
@@ -63,7 +73,7 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
             'query' => $query
         ];
     }
-    
+
     public function logError($message) {
         $this->queries[] = ['time' => '', 'memory' => '', 'query' => trim($message)];
     }
@@ -75,7 +85,7 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
                 . ($this->totalTime ? ' / ' . sprintf('%0.1f', $this->totalTime) . ' ms' : '')
                 . '</span>';
     }
-    
+
     public function getPanel() {
         $this->disabled = TRUE;
         $s = '';
@@ -90,10 +100,10 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
                 try {
                     $con = Propel::getConnection();
                     $cmd = is_string($this->explain) ? $this->explain : 'EXPLAIN';
-                    
+
                     $explain = array();
                     $result = $con->query("$cmd $sql");
-                    
+
                     while ($row = $result->fetch()) {
                         $explain[] = $row;
                     }
@@ -104,7 +114,7 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
 
             $s .= '<tr><td>';
             $s .= sprintf('%0.3f', $time);
-            
+
             if ($explain) {
                 static $counter;
                 $counter++;
@@ -127,11 +137,11 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
                 }
                 $s .= "</table>";
             }
-            
+
             $s .= '</td><td>' . $rows . '</td>';
             $s .= '<td>' . $memory . '</td></tr>';
         }
-        
+
         return $this->count ?
                 '<style class="nette-debug"> #nette-debug td.nette-DbConnectionPanel-sql { background: white !important }'
                 . ' #nette-debug .nette-DbConnectionPanel-source { color: #BBB !important } </style>'
@@ -139,7 +149,7 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
                 . ($this->totalTime ? ', time: ' . sprintf('%0.3f', $this->totalTime) . ' ms' : '') . ', ' . htmlSpecialChars($this->name) . '</h1>'
                 . '<div class="nette-inner nette-DbConnectionPanel">'
                 . '<table>'
-				.   '<tr><th>Time&nbsp;ms</th><th>SQL Query</th><th>Affected Rows</th><th>Memory</th></tr>'. $s
+                . '<tr><th>Time&nbsp;ms</th><th>SQL Query</th><th>Affected Rows</th><th>Memory</th></tr>' . $s
                 . '</table>'
                 . (count($this->queries) < $this->count ? '<p>...and more</p>' : '')
                 . '</div>' : '';
@@ -148,8 +158,7 @@ class PropelPanel extends AbstractHandler implements Nette\Diagnostics\IBarPanel
     public function handle(array $record) {
         if ($record['level'] == \Monolog\Logger::INFO) {
             $this->logQuery($record['message']);
-        }
-        else {
+        } else {
             $this->logError($record['message']);
         }
     }
